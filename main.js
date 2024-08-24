3406109514cd09ed2e8779811a55b69e00f9236a
Julie <juliy0214561@gmail.com>
2024-08-24 19:02:10 +0800

    [update] 更新swiper，頁碼問題尚未解決

--- main.js
+++ main.js
@@ -1,2 +1,46 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+var swiper = new Swiper(".mySwiper", {
+    breakpoints: {
+        992: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 24,
+        }
+    },
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar"
+      },
+      keyboard: {
+        enabled: true,
+      },
+      fadeEffect: {
+        crossFade: true
+      }
+  });
+
+  document
+    .querySelector(".prepage")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      var currentPage = swiper.activeIndex;
+      swiper.slideTo(currentPage-1, 0);
+      
+    });
+  document
+    .querySelector(".nextpage")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      var currentPage = swiper.activeIndex;
+      swiper.slideTo(currentPage+1, 0);
+    });
+  
