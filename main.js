961f3b765ce7e2b717320dacbe3968cdfe6056bb
Julie <juliy0214561@gmail.com>
2024-08-25 11:34:12 +0800

    [update] 更新頁碼

--- main.js
+++ main.js
@@ -2,45 +2,65 @@ import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 var swiper = new Swiper(".mySwiper", {
-    breakpoints: {
-        992: {
-          slidesPerView: 2,
-          spaceBetween: 48,
-        },
-        768: {
-          slidesPerView: 1,
-          spaceBetween: 24,
-        }
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 48,
     },
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 48,
     },
-    scrollbar: {
-        el: ".swiper-scrollbar"
-      },
-      keyboard: {
-        enabled: true,
-      },
-      fadeEffect: {
-        crossFade: true
-      }
-  });
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 24,
+    },
+  },
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  keyboard: {
+    enabled: true,
+  },
+  fadeEffect: {
+    crossFade: true,
+  },
+});
 
-  document
-    .querySelector(".prepage")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      var currentPage = swiper.activeIndex;
-      swiper.slideTo(currentPage-1, 0);
-      
-    });
-  document
-    .querySelector(".nextpage")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      var currentPage = swiper.activeIndex;
-      swiper.slideTo(currentPage+1, 0);
-    });
-  
+document.querySelector(".prepage").addEventListener("click", function (e) {
+  e.preventDefault();
+  var currentPage = swiper.activeIndex - 1;
+  // 更新頁碼
+  var page = document.querySelector(".page");
+  swiper.slideTo(currentPage, 0);
+});
+document.querySelector(".nextpage").addEventListener("click", function (e) {
+  e.preventDefault();
+  var currentPage = swiper.activeIndex + 1;
+
+  swiper.slideTo(currentPage, 0);
+});
+
+// Select the node that will be observed for mutations
+const targetNode = document.querySelector(".swiper-pagination");
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true};
+
+// Callback function to execute when mutations are observed
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (mutation.type === "childList") {
+      var page = document.querySelector(".page");
+      page.innerHTML = targetNode.innerHTML;
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
