04d308ff6a81d55b1f5fb8b3788d70e206d66bbe
Julie <juliy0214561@gmail.com>
2024-08-27 22:54:26 +0800

    [update] 更新內容頁

--- main.js
+++ main.js
@@ -23,48 +23,51 @@ var swiper = new Swiper(".mySwiper", {
   keyboard: {
     enabled: true,
   },
-  
 });
 
+if (document.querySelector(".prepage") != undefined) {
+  document.querySelector(".prepage").addEventListener("click", function (e) {
+    e.preventDefault();
+    var currentPage = swiper.activeIndex - 1;
 
-document.querySelector(".prepage").addEventListener("click", function (e) {
-  e.preventDefault();
-  var currentPage = swiper.activeIndex - 1;
-  
-  swiper.slideTo(currentPage, 0);
-});
-document.querySelector(".nextpage").addEventListener("click", function (e) {
-  e.preventDefault();
-  var currentPage = swiper.activeIndex + 1;
+    swiper.slideTo(currentPage, 0);
+  });
+}
+if (document.querySelector(".nextpage") != undefined) {
+  document.querySelector(".nextpage").addEventListener("click", function (e) {
+    e.preventDefault();
+    var currentPage = swiper.activeIndex + 1;
 
-  swiper.slideTo(currentPage, 0);
-});
+    swiper.slideTo(currentPage, 0);
+  });
+}
 
 // Select the node that will be observed for mutations
 const targetNode = document.querySelector(".swiper-pagination");
+if (targetNode != undefined) {
+  // 初始化頁數
+  var page = document.querySelector(".page");
+  page.innerHTML = targetNode.innerHTML;
 
-// 初始化頁數
-var page = document.querySelector(".page");
-      page.innerHTML = targetNode.innerHTML;
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true };
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true};
-
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      var page = document.querySelector(".page");
-      page.innerHTML = targetNode.innerHTML;
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        var page = document.querySelector(".page");
+        page.innerHTML = targetNode.innerHTML;
+      }
     }
-  }
-};
+  };
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}
 
 /**
  * 熱門文章的swiper
@@ -78,15 +81,14 @@ var mobileSwiper = new Swiper(".mobileSwiper", {
     372: {
       slidesPerView: 1,
       spaceBetween: 24,
-    }
-  }
+    },
+  },
 });
 
 /**
  * 品味生活的swiper
  */
 var lifeSwiper = new Swiper(".lifeSwiper", {
-  
   breakpoints: {
     992: {
       direction: "horizontal",
@@ -102,21 +104,23 @@ var lifeSwiper = new Swiper(".lifeSwiper", {
       direction: "vertical",
       slidesPerView: 3,
       spaceBetween: 0,
-    }
-  }
+    },
+  },
 });
 document.querySelector(".prepage-life").addEventListener("click", function (e) {
   e.preventDefault();
   var currentPage = lifeSwiper.activeIndex - 1;
-  
-  lifeSwiper.slideTo(currentPage, 0);
-});
-document.querySelector(".nextpage-life").addEventListener("click", function (e) {
-  e.preventDefault();
-  var currentPage = lifeSwiper.activeIndex + 1;
 
   lifeSwiper.slideTo(currentPage, 0);
 });
+document
+  .querySelector(".nextpage-life")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    var currentPage = lifeSwiper.activeIndex + 1;
+
+    lifeSwiper.slideTo(currentPage, 0);
+  });
 
 /**
  * 提案的swiper
@@ -126,12 +130,12 @@ var projectSwiper = new Swiper(".projectSwiper", {
     768: {
       slidesPerView: 2,
       spaceBetween: 48,
-      centeredSlides: true
+      centeredSlides: true,
     },
     372: {
       slidesPerView: 1,
       spaceBetween: 24,
-      centeredSlides: true
-    }
-  }
+      centeredSlides: true,
+    },
+  },
 });
